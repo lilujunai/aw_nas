f1d091391c3ddd68a457f027b95f072cbb0d25b8
TangChangcheng <tangcc1127@gmail.com>
2020-05-01 19:38:05 +0800

    Support voc and coco

---
+++ aw_nas/dataset/transform.py
@@ -0,0 +1,53 @@
+import torch
+
+from aw_nas.dataset.voc_data_augmentation import Preproc
+from aw_nas.utils.box_utils import match
+
+class TrainAugmentation:
+    def __init__(self, size, mean=(104, 117, 123), std=1.0):
+        """
+        Args:
+            size: the size the of final image.
+            mean: mean pixel value per channel.
+        """
+        self.mean = mean
+        self.size = size
+        self.preproc = Preproc(size, mean, 0.6)
+
+    def __call__(self, img, boxes, labels):
+        """
+        Args:
+            img: the output of cv.imread in RGB layout.
+            boxes: boundding boxes in the form of (x1, y1, x2, y2).
+            labels: labels of boxes.
+        """
+        return self.preproc(img, boxes, labels)
+
+
+class TestTransform:
+    def __init__(self, size, mean=0.0, std=1.0):
+        self.preproc = Preproc(size, mean, -1)
+
+    def __call__(self, image, boxes, labels):
+        return self.preproc(image, boxes, labels)
+
+
+class TargetTransform(object):
+    def __init__(self, priors, iou_threshold, variance):
+        self.priors = priors
+        self.threshold = iou_threshold
+        self.variance = variance
+
+    def __call__(self, target, width, height):
+        boxes, labels = target
+        # boxes[:, ::2] = boxes[:, ::2] / width
+        # boxes[:, 1::2] = boxes[:, 1::2] / height
+
+        num_priors = self.priors.size(0)
+        loc_t = torch.Tensor(1, num_priors, 4)
+        conf_t = torch.LongTensor(1, num_priors)
+        match(self.threshold, torch.tensor(boxes).float(), self.priors, self.variance, torch.tensor(labels),
+                loc_t, conf_t, 0)
+        loc_t = loc_t.squeeze(0)
+        conf_t = conf_t.squeeze(0)
+        return loc_t, conf_t
